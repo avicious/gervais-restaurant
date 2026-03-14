12a1e70d5263080fb309aa7cf9ff305e03819c24
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-14 20:25:23 +0530

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,71 @@
+import { Menu, UtensilsCrossed } from "lucide-react";
 import images from "../../constants/images";
 import "./Navbar.css";
+import { useState } from "react";
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.gervais} alt="" />
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gervais} alt="gervais logo" />
+      </div>
+
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className="app__navbar-smallscreen">
+        <Menu color="#fff" size={27} onClick={() => {}} />
+
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <UtensilsCrossed color="#fff" size={27} onClick={() => {}} />
+          <ul className="app__navbar-smallscreen-links">
+            <li className="p__opensans">
+              <a href="#home">Home</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#about">About</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#menu">Menu</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#awards">Awards</a>
+            </li>
+            <li className="p__opensans">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;

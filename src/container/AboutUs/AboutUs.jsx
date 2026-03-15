c25b22aa430de3bf24c873705b4e83da7beddc98
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-15 23:28:58 +0530

    about us done

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,6 +1,49 @@
 import { images } from "../../constants";
 import "./AboutUs.css";
 
-const AboutUs = () => <div>AboutUs</div>;
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="letter g" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h2 className="headtext__cormorant">About Us</h2>
+        <img src={images.spoon} alt="spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          adipisci reprehenderit eos perspiciatis accusamus consectetur iste ex.
+          Labore, quo at. Aspernatur neque mollitia vero minus explicabo
+          deleniti at ipsum ullam.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h2 className="headtext__cormorant">Our History</h2>
+        <img src={images.spoon} alt="spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          adipisci reprehenderit eos perspiciatis accusamus consectetur iste ex.
+          Labore, quo at. Aspernatur neque mollitia vero minus explicabo
+          deleniti at ipsum ullam.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default AboutUs;

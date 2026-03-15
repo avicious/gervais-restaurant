9c1dacc48fbb62894a26bbc5edae34c870545485
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-15 23:12:12 +0530

    header finished

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,9 +5,21 @@ import "./Header.css";
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+        velit repellat a repellendus? Vero illo sunt eos vitae sint dignissimos
+        iusto suscipit aspernatur. Assumenda adipisci aliquid laudantium!
+        Consectetur, omnis cupiditate!
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="food image" />
+    </div>
   </div>
 );
 

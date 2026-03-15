6177a57472fd1be9f51cc11a6fd09ab508b03741
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-15 23:00:35 +0530

    subheading added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,6 +1,14 @@
 import { images } from "../../constants";
+import { SubHeading } from "../../components";
 import "./Header.css";
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+    <div className="app__wrapper_img"></div>
+  </div>
+);
 
 export default Header;

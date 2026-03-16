00464c59f30abd4a73106289692449e8957bbe9f
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-16 21:52:36 +0530

    special menu jsx finished

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,81 +1,70 @@
-import images from './images';
+import images from "./images";
 
 const wines = [
-  {
-    title: 'Chapel Hill Shiraz',
-    price: '$56',
-    tags: 'AU | Bottle',
-  },
-  {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
-  },
-  {
-    title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
-  },
-  {
-    title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
-  },
-  {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
-  },
+  { id: 1, title: "Chapel Hill Shiraz", price: "$56", tags: "AU | Bottle" },
+  { id: 2, title: "Catena Malbee", price: "$59", tags: "AU | Bottle" },
+  { id: 3, title: "La Vieillw Rose", price: "$44", tags: "FR | 750 ml" },
+  { id: 4, title: "Rhino Pale Ale", price: "$31", tags: "CA | 750 ml" },
+  { id: 5, title: "Irish Guinness", price: "$26", tags: "IE | 750 ml" },
 ];
 
 const cocktails = [
   {
-    title: 'Aperol Sprtiz',
-    price: '$20',
-    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+    id: 1,
+    title: "Aperol Sprtiz",
+    price: "$20",
+    tags: "Aperol | Villa Marchesi prosecco | soda | 30 ml",
   },
   {
+    id: 2,
     title: "Dark 'N' Stormy",
-    price: '$16',
-    tags: 'Dark rum | Ginger beer | Slice of lime',
+    price: "$16",
+    tags: "Dark rum | Ginger beer | Slice of lime",
   },
   {
-    title: 'Daiquiri',
-    price: '$10',
-    tags: 'Rum | Citrus juice | Sugar',
+    id: 3,
+    title: "Daiquiri",
+    price: "$10",
+    tags: "Rum | Citrus juice | Sugar",
   },
   {
-    title: 'Old Fashioned',
-    price: '$31',
-    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+    id: 4,
+    title: "Old Fashioned",
+    price: "$31",
+    tags: "Bourbon | Brown sugar | Angostura Bitters",
   },
   {
-    title: 'Negroni',
-    price: '$26',
-    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+    id: 5,
+    title: "Negroni",
+    price: "$26",
+    tags: "Gin | Sweet Vermouth | Campari | Orange garnish",
   },
 ];
 
 const awards = [
   {
+    id: 1,
     imgUrl: images.award02,
-    title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    title: "Bib Gourmond",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
   },
   {
+    id: 2,
     imgUrl: images.award01,
-    title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    title: "Rising Star",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
   },
   {
+    id: 3,
     imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    title: "AA Hospitality",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
   },
   {
+    id: 4,
     imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    title: "Outstanding Chef",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
   },
 ];
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,6 +1,41 @@
 import { images, data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 import "./SpecialMenu.css";
 
-const SpecialMenu = () => <div>SpecialMenu</div>;
+const SpecialMenu = () => (
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu That Fits Your Palette" />
+      <h2 className="headtext__cormorant">Today's Special</h2>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine) => (
+            <p key={`wine-${wine.id}`}>{wine.title}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu" />
+      </div>
+
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu_menu_heading">Cocktail</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail) => (
+            <p key={`cocktail-${cocktail.id}`}>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">View More</button>
+    </div>
+  </div>
+);
 
 export default SpecialMenu;

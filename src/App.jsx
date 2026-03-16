a499d0af67288c21d915e9c731d7cf3395700d08
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-16 23:19:45 +0530

    app cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,7 @@ const App = () => {
       <Header />
       <AboutUs />
       <SpecialMenu />
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <FindUs />
-      <Footer />
+      
     </div>
   );
 };

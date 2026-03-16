d5d7dfbba9be02e80dcb34a044006cbe60f44318
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-16 21:32:10 +0530

    special menu setup

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,6 @@
-import React from 'react';
+import { images, data } from "../../constants";
+import "./SpecialMenu.css";
 
-import './SpecialMenu.css';
-
-const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
-);
+const SpecialMenu = () => <div>SpecialMenu</div>;
 
 export default SpecialMenu;

b9903428077e4a90ede82795bc19b715f7486f1d
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-17 23:57:40 +0530

    intro jsx complete

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import { Play, Pause } from 'lucide-react';
-import {meal} from "../../constants"
-import './Intro.css';
+import { Play, Pause } from "lucide-react";
+import { meal } from "../../constants";
+import "./Intro.css";
+import { useRef, useState } from "react";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
 
+  const handleVideo = () => {
+    setPlayVideo((prevPlay) => !prevPlay);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          onClick={handleVideo}
+          className="app__video-overlay_circle flex__center"
+        >
+          {playVideo ? (
+            <Pause color="#fff" size={30} />
+          ) : (
+            <Play color="#fff" size={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Intro;

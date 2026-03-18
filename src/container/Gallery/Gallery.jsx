ca2c3d1b0c7f0ea6b9e5bae6ec8b7a4beb89fa1d
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-18 23:58:09 +0530

    gallery jsx added

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,14 +1,59 @@
 import { images } from "../../constants";
 import { Camera, ArrowLeft, ArrowRight } from "lucide-react";
 import "./Gallery.css";
+import { useRef } from "react";
+
+const imageArr = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
 
 const Gallery = () => {
-  return <div className="app__gallery flex__center">
-    <div className="app__gallery-content">
-      
+  const scrollRef = useRef();
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <h2 className="headtext__cormorant">Photo Gallery</h2>
+        <p className="p__opensans" style={{ color: "#aaa", marginTop: "1rem" }}>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+          aliquam explicabo quos exercitationem odit at vitae omnis quis laborum
+          quaerat! Molestiae eos consequuntur praesentium non accusamus quos
+          nostrum hic nihil.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}></div>
+        <div className="app__gallery-images_arrow">
+          <ArrowLeft
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <ArrowRight
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
     </div>
-    <div className=""></div>
-  </div>;
+  );
 };
 
 export default Gallery;

80c0df77017aea9cea517e3b3bf3528bd888adb3
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-18 23:44:57 +0530

    gallery jsx setup

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import { Camera, ArrowLeft, ArrowRight } from "lucide-react";
+import "./Gallery.css";
 
-import './Gallery.css';
-
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => <div>Gallery</div>;
 
 export default Gallery;

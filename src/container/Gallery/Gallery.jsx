9d11df1d42498eff9a35ac6d94132c613aee8809
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-18 23:48:26 +0530

    gallery css setup

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,8 +1,14 @@
 import { images } from "../../constants";
-import { SubHeading } from "../../components";
 import { Camera, ArrowLeft, ArrowRight } from "lucide-react";
 import "./Gallery.css";
 
-const Gallery = () => <div>Gallery</div>;
+const Gallery = () => {
+  return <div className="app__gallery flex__center">
+    <div className="app__gallery-content">
+      
+    </div>
+    <div className=""></div>
+  </div>;
+};
 
 export default Gallery;

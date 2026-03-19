3046ceeb72319f082abb576f7cbc9e67c76f3200
Shubham Kumar <shubham.kr2951@gmail.com>
2026-03-19 23:54:54 +0530

    newsletter setup

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import { SubHeading } from "../SubHeading";
 
-import './Newsletter.css';
+import "./Newsletter.css";
 
-const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+const Newsletter = () => <div>Newsletter</div>;
 
 export default Newsletter;
